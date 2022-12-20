f5f5b553ec8ff2e8aea1689ffc3eb0db5f76019e
madhuchintha827 <chinthamadhu827@gmail.com>
2022-12-20 05:45:11 +0000

    solution

---
+++ src/components/CryptocurrencyTracker/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+import './index.css'
+import CryptocurrenciesList from '../CryptocurrenciesList'
+
+const CryptocurrencyTracker = () => (
+  <div className="bg-container">
+    <h1 className="heading">Cryptocurrency Tracker</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+        alt="cryptocurrency"
+        className="image"
+      />
+    </div>
+    <CryptocurrenciesList />
+  </div>
+)
+export default CryptocurrencyTracker
